9ee80d8e1e910dc9d092368fa69893f8f08292da
wladiolo <wladiolo@lavabit.com>
2019-05-29 18:13:24 +0200

    Added options to search tech or headers

--- README.md
+++ README.md
@@ -8,8 +8,12 @@ It can be useful when you need to found a specific technology in a massive scan 
 
 ## How to use webtech_search
 ```
-webtech_search.sh file-to-parse tecnology-to-search
+webtech_search.sh file-to-parse options string-to-search
 ```
+Where *options* are:
+
+--tech: search in the *tech* array
+--head: search in the *headers* array
 
 ## Example
 First do a scan with **webtech**
@@ -18,9 +22,14 @@ webtech --urls-file=urls_to_scan.list --json >> output.json
 ```
 Then search for a specific technology with **webtech_search** 
 ```
-webtech_search.sh output.json woRDpr
+webtech_search.sh output.json -tech woRDpr
 Site: https://www.example.com Technology: Wordpress Version: 5.1.2
 Site: https://www.other.com Technology: WordPress Version: 3.1.1
+```
+Or search for a specific header:
+```
+webtech_search.sh output.json -head tRAc
+Site: https://www.example.com Header: X-Cloud-Trace-Context
 ```
 
 ## Thanks to
--- webtech_search.sh
+++ webtech_search.sh
@@ -1,35 +1,58 @@
 #!/bin/bash
 # wladiolo (https://github.com/wladiolo)
-# This script parses the json output of webtech tool (https://github.com/ShielderSec/webtech) to find a specific technology
+# This script parses the json output of webtech tool (https://github.com/ShielderSec/webtech) to find a specific technology or a specific header
 # It requires jq (https://stedolan.github.io/jq/) to function properly
 # usage:
-# webtech_search.sh <file to parse> <tecnology to search>
+# webtech_search.sh <file to parse> <options> <string>
+# Options:
+# --tech: search for string in the tech array
+# --head: search for string in the headers array
 
 # colors, for a better world
 BLUE='\e[0;34m'
 YELLOW='\e[0;33m'
 PURPLE='\e[0;35m'
 
-if [[ $# -eq 0 || $# -eq 1 ]]; then
-	echo -e "usage: webtech_search.sh <file to parse> <tecnology to search>"
+if [[ $# -le 2 ]]; then
+	echo -e "usage: webtech_search.sh <file to parse> <options> <string>"
+	echo -e "Options:"
+	echo -e "--tech: search for string in the tech array"
+	echo -e "--head: search for string in the headers array"
 	exit
 fi
 
 # arguments parsing
 input_file=$1
-tech_to_search=$2
+option=$2
+string_to_search=$3
 
 # sites are the keys in the json file
 for url in $( cat $input_file | jq -r '.|keys|.[]' ); do
-	# tech is an array
-	for t in $( cat $input_file | jq -r --arg sito "$url" '.[$sito].tech|keys|.[]' ); do
-		# tech and version extraction
-		tech=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.tech[$id].name' )
-		vers=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.tech[$id].version' )
-		# match the string "tech_to_search" (uppercase forced to do a case independent match)
-		if [[ "${tech^^}" =~ "${tech_to_search^^}" ]]; then
-			echo -e "${BLUE}Site: $url ${YELLOW}Technology: $tech ${PURPLE}Version: $vers"
-		fi
-	done
+	# do a tech searh
+	if [[ "$option" == "--tech" ]]; then
+		# tech is an array
+		for t in $( cat $input_file | jq -r --arg sito "$url" '.[$sito].tech|keys|.[]' ); do
+			# tech and version extraction
+			tech=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.tech[$id].name' )
+			vers=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.tech[$id].version' )
+			# match the string "string_to_search" (uppercase forced to do a case independent match)
+			if [[ "${tech^^}" =~ "${string_to_search^^}" ]]; then
+				echo -e "${BLUE}Site: $url ${YELLOW}Technology: $tech ${PURPLE}Version: $vers"
+			fi
+		done
+	fi
+	# do a header search
+	if [[ "$option" == "--head" ]]; then
+		#headers is an array
+		for t in $( cat $input_file | jq -r --arg sito "$url" '.[$sito].headers|keys|.[]' ); do
+                        # header and value extraction
+                        head=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.headers[$id].name' )
+                        value=$( cat $input_file | jq -r --arg sito "$url" --argjson id "$t" '.[$sito]|.headers[$id].value' )
+                        # match the string "string_to_search" (uppercase forced to do a case independent match)
+                        if [[ "${head^^}" =~ "${string_to_search^^}" ]]; then
+                                echo -e "${BLUE}Site: $url ${YELLOW}Header: $head ${PURPLE}Value: $value"
+                        fi
+                done
+	fi
 done
 															
